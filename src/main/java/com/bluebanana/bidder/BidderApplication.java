5f0a4ded18413d3b402fa2acf0469327f264d0c4
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-12-19 10:47:57 +0200

    Move JavaConfig to separate class from BidderApplication.java

--- src/main/java/com/bluebanana/bidder/BidderApplication.java
+++ src/main/java/com/bluebanana/bidder/BidderApplication.java
@@ -2,10 +2,8 @@ package com.bluebanana.bidder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class BidderApplication {
 
     public static void main(String[] args) {

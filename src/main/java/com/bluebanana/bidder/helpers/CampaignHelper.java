2b77056d509254a75c1dc051e54ad41c30860491
Vasilis Lampropoulos <lampropoul1989@gmail.com>
2022-09-07 12:25:57 +0300

    Code refinements, part 2

--- src/main/java/com/bluebanana/bidder/helpers/CampaignHelper.java
+++ src/main/java/com/bluebanana/bidder/helpers/CampaignHelper.java
@@ -1,16 +1,14 @@
 package com.bluebanana.bidder.helpers;
 
-import com.bluebanana.bidder.models.Campaign;
-import com.bluebanana.bidder.pacing.Pacing;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import com.bluebanana.bidder.models.Campaign;
+import com.bluebanana.bidder.pacing.Pacing;
+import lombok.RequiredArgsConstructor;
 
 /**
  * This class holds all helper methods for the Campaigns
@@ -32,22 +30,17 @@ public class CampaignHelper {
      * or an empty Campaign if no Campaign that matches the criteria was found
      */
     public Optional<Campaign> getHighestPayingCampaign(String country) throws IOException {
-        List<Campaign> campaignList =
+        return
             Arrays.stream(api.getAllCampaigns())
                   .filter(campaign -> campaign.getTargetedCountries().contains(country))
-                  .sorted((campaign1, campaign2) -> Double.compare(campaign2.getPrice(), campaign1.getPrice())) // reverse sort (DESC)
                   .filter(campaign -> pacing.campaignDidNotReachPacingLimit(campaign.getId()))
-                  .collect(Collectors.toList());
-        
-        if (campaignList.isEmpty()) {
-            return Optional.empty();
-        }
-        else {
-            Campaign campaign = campaignList.get(0);
-            Integer numOfBids = pacing.getCampaignsToNumOfBids().get(campaign.getId());
-            pacing.getCampaignsToNumOfBids().replace(campaign.getId(), ++numOfBids);
-            return Optional.of(campaign);
-        }
+                  .min((campaign1, campaign2) -> Double.compare(campaign2.getPrice(), campaign1.getPrice())) // reverse sort (DESC)
+                  .map(campaign -> {
+                      Integer numOfBids = pacing.getCampaignsToNumOfBids().get(campaign.getId());
+                      pacing.getCampaignsToNumOfBids().replace(campaign.getId(), ++numOfBids);
+                      return Optional.of(campaign);
+                  })
+                  .orElse(Optional.empty());
     }
     
 }
